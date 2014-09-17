84019b58a50a8d9c2b7283d88732d3b335fb27bb
Vincent-Echo <xiewenbin@outlook.com>
2014-09-17 17:26:55 +0800

    using py2exe create HtmlToImage.pyw to an exe file
    使用Py2exe来转化python文件为windows下exe文件

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+__author__ = 'wxie'
+
+from distutils.core import setup
+import sys
+import py2exe
+
+sys.path.append('C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/redist/x86/Microsoft.VC90.CRT')
+
+setup(windows=[{"script": "HtmlToImage.pyw"}],
+      options={"py2exe": {"includes": ["sip"]}},
+      data_files = [
+                ('imageformats', [
+                            'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qjpeg4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qgif4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qico4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qmng4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qsvg4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtga4.dll',
+                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtiff4.dll',
+                            ])
+                ]
+)
