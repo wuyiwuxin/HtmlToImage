1af0f5d9c41ac175f9ea62fbbc64d3878cf9bc05
Vincent-Echo <xiewenbin@outlook.com>
2014-09-17 17:39:56 +0800

    For good readbility
    把图片格式文件路径单独存放，方便修改

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ import py2exe
 
 sys.path.append('C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/redist/x86/Microsoft.VC90.CRT')
 
+
+image_format = [('imageformats', [
+                    'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qjpeg4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qgif4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qico4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qmng4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qsvg4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtga4.dll',
+                    #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtiff4.dll',
+                ])]
+
+
 setup(windows=[{"script": "HtmlToImage.pyw"}],
       options={"py2exe": {"includes": ["sip"]}},
-      data_files = [
-                ('imageformats', [
-                            'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qjpeg4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qgif4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qico4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qmng4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qsvg4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtga4.dll',
-                            #'C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qtiff4.dll',
-                            ])
-                ]
-)
+      data_files=image_format)
